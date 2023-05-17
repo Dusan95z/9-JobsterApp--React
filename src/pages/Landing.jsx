a142bcfabbecc2234a1d628f601527989117623f
Dusan95z <dusan204z@gmail.com>
2023-05-17 12:29:19 +0200

    login and register

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 
@@ -19,7 +20,9 @@ const Landing = () => {
               asperiores accusantium eligendi, maiores atque esse ipsum qui
               velit ex sapiente?
             </p>
-            <button className='btn btn-hero'>Login / Register</button>
+            <Link to='/register' className='btn btn-hero'>
+              Login / Register
+            </Link>
           </div>
           <img src={main} alt='job hunt' className='img main-img' />
         </div>
